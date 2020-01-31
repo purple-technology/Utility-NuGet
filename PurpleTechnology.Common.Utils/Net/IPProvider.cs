4df3e237f96d8f7fbb6099e3a68f61a7d5c5eaf1
KUTlime <kutlime@gmail.com>
2020-01-31 15:38:05 +0100

    GetPublicIPAddress implementation.

---
+++ PurpleTechnology.Common.Utils/Net/IPProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Net;
+
+namespace PurpleTechnology.Common.Utils.Net
+{
+    /// <summary>
+    /// An IP address utilities.
+    /// </summary>
+    public class IPProvider
+    {
+        /// <summary>
+        /// Returns the client's Internet public address.
+        /// <para>If network connection is unavailable or remote API unreachable, IPAddress.None is returned.</para>
+        /// </summary>
+        /// <returns>The client internet public address.</returns>
+        public static IPAddress GetPublicIpAddress()
+        {
+            IPAddress ipAddress = IPAddress.None;
+            try
+            {
+                ipAddress = IPAddress.Parse(new WebClient().DownloadString("https://api.ipify.org"));
+            }
+            catch
+            {
+                // Exceptions are swallowed, IPAddress.None (255.255.255.255) is returned.
+            }
+            return ipAddress;
+        }
+    }
+}
